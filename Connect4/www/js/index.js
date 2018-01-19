532bfccf58c547c83b3034edccfef2d33c83dc33
Sangfer <alexis.barthelemy02@gmail.com>
2018-01-19 16:14:24 +0100

    Functionnal color implementation of tokens

--- Connect4/www/js/index.js
+++ Connect4/www/js/index.js
@@ -1,9 +1,52 @@
+var tour=0;
+var tokenWhite='<img src="../img/token_white.png" style="width:50px; height:auto;">';
+var tokenRed='<img src="../img/token_red.png" style="width:50px; height:auto;">';
+var tokenYellow='<img src="../img/token_yellow.png" style="width:50px; height:auto;">';
+
     var initElement = function () {
-        alert("coucou");
+      $('.myTable tr').each(function() {
+          $.each(this.cells, function(){
+            $(this).css('background-color', 'white');
+
+          });
+      });
     };
 
+
+  //   $('.myTable tr td').on("click",function() {
+  //         $(this).css('background-color', 'red');
+  //         $(this).text("2");
+  //   });
+  // }
+
+
+
+
+
+$('.myTable tr td').on("click",function() {
+    tour++;
+
+                var col=$(this).closest("td").index();
+                var col=col+1;
+                for(var row = 6; row >= 1; row--){
+                     if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenWhite) {
+                         if(tour%2==0){
+                             $('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html(tokenRed);
+                             break;
+                         } else {
+                             $('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html(tokenYellow);
+                             break;
+                         }
+
+                     }
+                }
+          });
+
+
+
+
     var _deviceReadyCallback = function () {
-        $(".drawGrid").off("click").on("click", initElement);
+        initElement();
     };
 
     document.addEventListener('deviceready', _deviceReadyCallback, false);
