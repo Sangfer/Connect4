e24a86bf2f8fa99ae2ef0f48a1041b05d784bba5
Sangfer <alexis.barthelemy02@gmail.com>
2018-01-20 00:20:01 +0100

    Working code, font added, design over, comments added

--- Connect4/www/js/index.js
+++ Connect4/www/js/index.js
@@ -1,7 +1,7 @@
 var turn=0;
-var tokenWhite='<img src="../img/token_white.png" style="width:50px; height:auto;">';
-var tokenRed='<img src="../img/token_red.png" style="width:50px; height:auto;">';
-var tokenYellow='<img src="../img/token_yellow.png" style="width:50px; height:auto;">';
+var tokenWhite='<img src="../img/token_white.png" class="myCell">';
+var tokenRed='<img src="../img/token_red.png" class="myCell">';
+var tokenYellow='<img src="../img/token_yellow.png" class="myCell">';
 
 var counterRedHorizontal =0;
 var counterRedVertical = 0;
@@ -12,22 +12,22 @@ var scoreYellow = 0;
 var scoreRed = 0;
 
 
+/***
+ * Is Called when the app is launched
+ * Set the score for Yellow and Red and give Yellow's the first move
+ */
 var initGame = function () {
     $('.scoreYellow').html('Yellow '+scoreYellow);
     $('.scoreRed').html('Red '+scoreRed);
     $('.turn').html('Yellow\'s turn to play:').css('color', 'yellow');
-    // $('.myTable tr').each(function() {
-    //     $.each(this.cells, function(){
-    //         $(this).css('background-color', 'white');
-    //
-    //     });
-    // });
 };
 
-
+/**
+ * Reset the game to the Original step. Turn all cells into blank, and save the score.
+ */
 var resetGame = function() {
     for (var col = 1; col < 8; col++) {
-        for (var row = 5; row >= 0; row--) {
+        for (var row = 6; row >= 1; row--) {
             $('.myTable tr:nth-child(' + row + ') td:nth-child(' + col + ')').html(tokenWhite);
         }
     }
@@ -38,6 +38,12 @@ var resetGame = function() {
     $('.turn').html('Yellow\'s turn to play:').css('color', 'yellow');
 };
 
+/**Check if the game is over. Means multiple things as the function's name suggest:
+ * - if the game is a draw
+ * - if yellow wins with rows or cells
+ * - if red wins with rows or cell.
+ * Upgrade the score and reset the game.
+ */
 
 var checkForEndOfTheGame = function(){
     if (checkIfGameIsNull()==-1){
@@ -73,45 +79,64 @@ var checkForEndOfTheGame = function(){
     }
 };
 
-
+/**
+ * Check if the game is a draw, means if the first row does not include blank tokens
+ * @returns {*-1 if the game is a draw, else 0}
+ */
 var checkIfGameIsNull = function() {
     var x;
     for(var col = 1; col <8; col++){
         if( $('.myTable tr:nth-child(1) td:nth-child('+col+')').html()==tokenWhite){
+            //Check for the top row if therer's a white token, means there's still one move minimum to be played.
             x=0;
             return x;
         } else {
+            //Means the row is full of color, so the game is a draw
             x=-1;
         }
     }
     return x;
 };
 
-
+/**
+ * Check Column by Column if there is a win, means:
+ * If a token isn't white
+ * If there's 4 successive token from a same color
+ * @returns {number}
+ */
 var checkIfColumnIsWinning = function(){
 
     for(var col = 1; col <8; col++){
-        for(var row =5; row >= 0; row--){
+        for(var row =6; row >= 1; row--){
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenWhite){
                 counterRedVertical=0;
                 counterYellowVertical=0;
+                //If there's a white token, then means that the previous combo is broken
             }
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenRed){
+                //If there's a red, then red put a token out of 4, so we increment the counter of red
+                //and reset yellow's one to 0.
                 counterRedVertical++;
                 counterYellowVertical=0;
             }
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenYellow){
+                //If there's a yellow, then yellow put a token out of 4, so we increment the counter of yellow
+                //and reset red's one to 0.
                 counterRedVertical=0;
                 counterYellowVertical++;
             }
             if(counterRedVertical==4){
+                //means win of Red by columns
                 return 2;
             }
             if(counterYellowVertical==4){
+                //means win of Yellow by columns
                 return 1;
             }
         }
-
+        //Reset the counter for Yellow and Red Columns in order not to outreach on others columns.
+        counterYellowVertical=0;
+        counterRedVertical=0;
     }
     return 0;
 };
@@ -120,31 +145,35 @@ var checkIfColumnIsWinning = function(){
 
 var checkIfRowIsWinning = function(){
 
-    for(var row =5; row >= 0; row--){
+    for(var row =6; row >= 1; row--){
         for(var col = 1; col <8; col++){
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenWhite){
+                //If there's a white token, then means that the previous combo is broken
                 counterRedHorizontal=0;
                 counterYellowHorizontal=0;
             }
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenRed){
+                //If there's a red, then red put a token out of 4, so we increment the counter of red
+                //and reset yellow's one to 0.
                 counterRedHorizontal++;
                 counterYellowHorizontal=0;
             }
             if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenYellow){
+                //If there's a yellow, then yellow put a token out of 4, so we increment the counter of yellow
+                //and reset red's one to 0.
                 counterRedHorizontal=0;
                 counterYellowHorizontal++;
             }
             if(counterRedHorizontal==4){
-                counterYellowHorizontal=0;
-                counterRedVertical=0;
+                //means win of Red by rows
                 return 2;
             }
             if(counterYellowHorizontal==4){
-                counterYellowHorizontal=0;
-                counterRedVertical=0;
+                //means win of Yellow by rows
                 return 1;
             }
         }
+        //Reset the counter for Yellow and Red Columns in order not to outreach on others columns.
         counterYellowHorizontal=0;
         counterRedVertical=0;
     }
@@ -154,19 +183,27 @@ var checkIfRowIsWinning = function(){
 
 
 $('.myTable tr td').on("click",function() {
-    turn++;
     var col=$(this).closest("td").index();
     var col=col+1;
+    //Get the Columns number
     for(var row = 6; row >= 1; row--){
+        //for each row beginning by the last one
         if($('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html()==tokenWhite) {
+            //if my token is White, then I can play, in the other case, we are going to the same column but on the row upper.
+            turn++;
             if(turn%2==0){
+                //Checker for knowing which turn it is to play.
                 $('.turn').html('Yellow\'s turn to play:').css('color', 'yellow');
+                //Set the token to red
                 $('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html(tokenRed);
+                //check for all possible results
                 checkForEndOfTheGame();
                 break;
             } else {
                 $('.turn').html('Red\'s turn to play:').css('color', 'red');
+                //set the token to yellow
                 $('.myTable tr:nth-child('+ row +') td:nth-child('+ col +')').html(tokenYellow);
+                //check for all possible results
                 checkForEndOfTheGame();
                 break;
             }
